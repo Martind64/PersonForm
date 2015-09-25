53776fcb00a1b2dfa2415e3bb0c4d69541afafc1
Martind64 <Martind_64@hotmail.com>
2015-09-25 12:04:56 +0200

    addPerson Action was added

--- src/AppBundle/Form/Type/PersonType.php
+++ src/AppBundle/Form/Type/PersonType.php
@@ -1,7 +1,11 @@
 <?php
+
+
 namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+
+
 class PersonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
